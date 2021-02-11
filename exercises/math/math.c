8c66fa59ab562b99a148794da7e3683d3275e33a
Anton <antonrindomkristensen123@gmail.com>
2021-02-11 15:31:17 +0100

    Ooops I forgot the last part of the math exercise

--- exercises/math/math.c
+++ exercises/math/math.c
@@ -6,6 +6,8 @@
 
 int main(void){
 
+
+// First part of the exercise, some various functions and complex numbers
 printf("Gamma(5) =  %g, \n", tgamma(5)); //tgamma(x) spytter en double ud, så jeg kan ikke bar ebruge %i, men bliver nødt til %g
 
 printf("J_1(0.5) = %g \n",j1(0.5)); //Nu bruger vi doubles, for vi skal tage besselfunktionen af en halv
@@ -24,6 +26,15 @@ printf("i^e = %g%+gi \n",creal(ie),cimag(ie));
 
 double complex ii = cpow(I,I);
 printf("i^i = %g%+gi \n",creal(ii),cimag(ii));
+
+
+
+// Second part of the exercise, some precision stuff.
+printf("Float 1/9 = %.25g \n",1.f/9);
+printf("Double 1/9 = %.25lg \n",1./9);
+printf("Long double 1/9 = %.25Lg \n",1.L/9);
+
+printf("It should of course just be endles 0.1111.....");
 
 return 0;
 }
